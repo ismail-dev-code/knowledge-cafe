01bb58309da44cea22486cb2d4d3b748ea6ddc9d
ismail-dev-code <ismail.online0385678@gmail.com>
2025-04-06 09:36:04 +0600

    bookmark btn added

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import { FaBookmark } from "react-icons/fa";
 
 const Blog = ({blog}) => {
     // const {blog} = props;
 
     console.log(blog);
     return (
-        <div>
+        <div className='m-10'>
            <div className="card bg-base-100 w-96 shadow-sm">
   <figure>
     <img
@@ -13,8 +14,18 @@ const Blog = ({blog}) => {
       alt="cover pics" />
   </figure>
   <div className="card-body">
+    <div className="author flex justify-around items-center">
+        <h3>{blog.author}</h3>
+        <img className='w-16' src={blog.author_img} alt="" />
+        <FaBookmark size={25} />
+    </div>
     <h2 className="card-title">{blog.title}</h2>
-    <p>{blog.author_img}</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id, hic.</p>
+    <div className="flex">
+    {
+      blog.hashtags.map((hash=><p>{hash}</p>))
+    }
+    </div>
     <div className="card-actions justify-end">
       <button className="btn btn-primary">Mark AS Read</button>
     </div>
